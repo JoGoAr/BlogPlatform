32b13b6270c54da495fba5a42c766fea3e2dfe36
JoeGomez1 <jgomezarellano@my.waketech.edu>
2026-03-25 18:21:10 -0400

    Working blog platform: post list and detail pages functional

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(published=True)
+    return render(request, 'post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk, published=True)
+    return render(request, 'post_detail.html', {'post': post})
